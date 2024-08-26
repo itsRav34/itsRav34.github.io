12aadf50062facb4b69ac0736b1efdc55f111e39
Ravi L <93421336+itsRav34@users.noreply.github.com>
2024-08-26 15:56:29 +1000

    updated code with personal info

--- src/info/Info.js
+++ src/info/Info.js
@@ -8,7 +8,7 @@ import mock5 from "../img/mock5.png"
 /* Hi there! Thanks for checking out my portfolio template. Be sure to read the comments to get a better understanding of
 how to make this template work best for you! */
 
-export let colors = ["rgb(0,255,164)", "rgb(166,104,255)"];
+export let colors = ["rgb(149, 217, 218)", "rgb(109, 214, 218)"];
 /*
 I highly recommend using a gradient generator like https://gradientgenerator.paytonpierce.dev/ to generate a pair of colors that you like.
 These colors will be used to style your name on the homepage, the background of your picture, and some other accents throughout
@@ -27,10 +27,10 @@ of what each of the values mean.
  */
 
 export const info = {
-    firstName: "John",
-    lastName: "Smith",
-    initials: "js", // the example uses first and last, but feel free to use three or more if you like.
-    position: "a Full Stack Developer",
+    firstName: "Ravi",
+    lastName: "Liyanaarachchi",
+    initials: "RL", // the example uses first and last, but feel free to use three or more if you like.
+    position: "a Mechatronics Engineer",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
     baseColor: colors[0],
@@ -41,52 +41,42 @@ export const info = {
         },
         {
             emoji: '🌎',
-            text: 'based in the US'
+            text: 'based in the Sydney, AU'
         },
         {
             emoji: "💼",
-            text: "Systems Engineer at Google"
+            text: "Intern Engineer at Bromic"
         },
         {
             emoji: "📧",
-            text: "johnsmith@gmail.com"
+            text: "ravindra.2000@hotmail.com"
         }
     ],
     socials: [
         {
-            link: "https://facebook.com",
+            link: "https://www.facebook.com/ravi.liyanaarachchi/",
             icon: 'fa fa-facebook',
             label: 'facebook'
         },
         {
-            link: "https://instagram.com",
-            icon: 'fa fa-instagram',
-            label: 'instagram'
-        },
-        {
-            link: "https://github.com",
+            link: "https://github.com/itsRav34",
             icon: "fa fa-github",
             label: 'github'
         },
         {
-            link: "https://linkedin.com",
+            link: "https://linkedin.com/in/ravil34/",
             icon: "fa fa-linkedin",
             label: 'linkedin'
-        },
-        {
-            link: "https://twitter.com",
-            icon: "fa fa-twitter",
-            label: 'twitter'
         }
 // Feel free to remove any of these that you don't have. I'll write an FAQ on how to add new ones later, let me know if you have a request for an icon!
 // Just change the links so that they lead to your social profiles.
 
     ],
-    bio: "Hello! I'm John. I'm a systems engineer for Google. I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
+    bio: "Hey there! I'm Ravi. I'm a Mechatronic Engineering (Hons) and Medical Science Student about to finish my degree! I studied CompSci at Harvard, I enjoy long walks on the beach, and I believe artificial intelligence will inevitably rule us all one day. You should hire me!",
     skills:
         {
-            proficientWith: ['javascript', 'react', 'git', 'github', 'bootstrap', 'html5', 'css3', 'figma'],
-            exposedTo: ['nodejs', 'python', 'adobe illustrator']
+            proficientWith: ['C++', 'SOLIDWORKS (+PDM)', 'ROS', 'MATLAB', '3D Printing', 'github', 'Arudino', 'Microsoft Office Suite'],
+            exposedTo: ['Fusion 360', 'python', 'Adobe Suite', 'Mathematica']
         }
     ,
     hobbies: [
@@ -95,16 +85,16 @@ export const info = {
             emoji: '📖'
         },
         {
-            label: 'theater',
-            emoji: '🎭'
+            label: 'Gym',
+            emoji: '🏋️‍♂️'
         },
         {
-            label: 'movies',
-            emoji: '🎥'
+            label: 'PC Gaming',
+            emoji: '🖱️'
         },
         {
-            label: 'cooking',
-            emoji: '🌶'
+            label: 'F1',
+            emoji: '🏎️'
         }
 // Same as above, change the emojis to match / relate to your hobbies or interests.
 // You can also remove the emojis if you'd like, I just think they look cute :P
